3955f17a463a0093f7a439bbe921a7b2f5d0aea2
Yoshida Hiroshi <yoshida@shiguredo.jp>
2025-10-28 11:35:47 +0900

    - 使用する python のバージョンを指定する - python のバージョンを指定することで追加したパッケージは不要になったため削除する

--- ingester/run.sh
+++ ingester/run.sh
@@ -2,7 +2,7 @@
 
 DEBIAN_FRONTEND=noninteractive
 
-apt-get -y update && apt-get -y install curl python3 g++ libffi-dev
+apt-get -y update && apt-get -y install curl python3
 
 # UTC ではなく /UTC にリンクが貼られ、DuckDB の TimeZone 設定も /UTC になるため、
 # Python API 側で UnknownTimeZoneError になるため、リンクを UTC に変更する
