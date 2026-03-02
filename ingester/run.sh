976d4917037f47f0a1c49dd448a2dd38df05e53b
Yoshida Hiroshi <yoshida@shiguredo.jp>
2026-03-02 11:20:57 +0900

    変数を "${...}" で統一する

--- ingester/run.sh
+++ ingester/run.sh
@@ -19,13 +19,13 @@ cd /ingester
 rm -rf ./.venv
 uv sync
 # テーブル作成および初期データの挿入
-uv run python src/run.py --db $DUCKDB_DB_PATH \
-                      --storage $STORAGE \
-                      --s3_endpoint $S3_ENDPOINT \
-                      --s3_access_key_id $AWS_ACCESS_KEY_ID \
-                      --s3_secret_access_key $AWS_SECRET_ACCESS_KEY \
-                      --s3_bucket $S3_BUCKET \
-                      --s3_prefix $S3_PREFIX \
+uv run python src/run.py --db "${DUCKDB_DB_PATH}" \
+                      --storage "${STORAGE}" \
+                      --s3_endpoint "${S3_ENDPOINT}" \
+                      --s3_access_key_id "${AWS_ACCESS_KEY_ID}" \
+                      --s3_secret_access_key "${AWS_SECRET_ACCESS_KEY}" \
+                      --s3_bucket "${S3_BUCKET}" \
+                      --s3_prefix "${S3_PREFIX}" \
                       init
 
 [ $? -ne 0 ] && echo "Failed to initialize the database." && exit 1
@@ -34,22 +34,22 @@ uv run python src/run.py --db $DUCKDB_DB_PATH \
 # 定期的にデータを更新
 while :;
 do
-  uv run python src/run.py --db $DUCKDB_DB_PATH \
-                        --storage $STORAGE \
-                        --s3_endpoint $S3_ENDPOINT \
-                        --s3_access_key_id $AWS_ACCESS_KEY_ID \
-                        --s3_secret_access_key $AWS_SECRET_ACCESS_KEY \
-                        --s3_bucket $S3_BUCKET \
-                        --s3_prefix $S3_PREFIX \
+  uv run python src/run.py --db "${DUCKDB_DB_PATH}" \
+                        --storage "${STORAGE}" \
+                        --s3_endpoint "${S3_ENDPOINT}" \
+                        --s3_access_key_id "${AWS_ACCESS_KEY_ID}" \
+                        --s3_secret_access_key "${AWS_SECRET_ACCESS_KEY}" \
+                        --s3_bucket "${S3_BUCKET}" \
+                        --s3_prefix "${S3_PREFIX}" \
                         update
 
   [ $? -ne 0 ] && echo "Failed to update the database." && exit 1
 
-  uv run python src/run.py --db $DUCKDB_DB_PATH \
-                        --retention_period $RETENTION_PERIOD \
+  uv run python src/run.py --db "${DUCKDB_DB_PATH}" \
+                        --retention_period "${RETENTION_PERIOD}" \
                         delete
 
   [ $? -ne 0 ] && echo "Failed to delete old logs." && exit 1
 
-  sleep $UPDATE_INTERVAL
+  sleep "${UPDATE_INTERVAL}"
 done
