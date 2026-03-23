a0954cab62c3da227e8c80536c336e904bc34f51
Yoshida Hiroshi <yoshida@shiguredo.jp>
2026-03-23 15:29:34 +0900

    S3_USE_SSL のみで http, https の利用を判別できるため S3_ENDPOINT_SCHEME を削除する

--- ingester/run.sh
+++ ingester/run.sh
@@ -10,6 +10,11 @@ mkdir -p /var/lib/kohaku/duckdb
 
 cd /ingester
 
+s3_ssl_args=()
+if [ "${S3_USE_SSL:-}" = "true" ]; then
+  s3_ssl_args+=(--s3_use_ssl)
+fi
+
 # テーブル作成および初期データの挿入
 if ! uv run python src/run.py --db "${DUCKDB_DB_PATH}" \
                            --storage "${STORAGE}" \
@@ -18,6 +23,7 @@ if ! uv run python src/run.py --db "${DUCKDB_DB_PATH}" \
                            --s3_secret_access_key "${AWS_SECRET_ACCESS_KEY}" \
                            --s3_bucket "${S3_BUCKET}" \
                            --s3_prefix "${S3_PREFIX}" \
+                           "${s3_ssl_args[@]}" \
                            init; then
   echo "run.py init failed. continue to update loop." >&2
 fi
@@ -33,6 +39,7 @@ do
                              --s3_secret_access_key "${AWS_SECRET_ACCESS_KEY}" \
                              --s3_bucket "${S3_BUCKET}" \
                              --s3_prefix "${S3_PREFIX}" \
+                             "${s3_ssl_args[@]}" \
                              update; then
     echo "run.py update failed. continue loop." >&2
   fi
