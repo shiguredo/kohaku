36422d7830d6482571cc5b5adba57dc7d12067d3
Yoshida Hiroshi <yoshida@shiguredo.jp>
2026-06-30 11:57:30 +0900

    ingester/run.sh の S3_REGION デフォルトの意図的な二重管理をコメントで明示する

--- ingester/run.sh
+++ ingester/run.sh
@@ -22,6 +22,10 @@ umask 0002
 
 cd /ingester
 
+# S3_REGION のデフォルト ap-northeast-1 は compose.yml / compose.external-s3.yml にも
+# 同じ値が定義されている (意図的な二重管理)。 compose 経由ではコンテナに必ず値が渡るため
+# 本ファイルの :-ap-northeast-1 は実質未到達だが、 念のため compose 側とデフォルトを揃える。
+
 s3_ssl_args=()
 if [ "${S3_USE_SSL:-}" = "true" ]; then
   s3_ssl_args+=(--s3_use_ssl)
